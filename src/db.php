5cd6b28a73622d3f790c37576a8422f413ec1805
Enma48 <ngopipea12@gmail.com>
2025-07-25 19:50:34 +0700

    Fixing error in setup_db.php

--- src/db.php
+++ src/db.php
@@ -5,10 +5,8 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'vul_db');
 
-// Buat koneksi
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Cek koneksi
 if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
