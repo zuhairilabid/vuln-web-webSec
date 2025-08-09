6e654d3b17a39561b19b7f53fd33a3a488709623
Nabil Arya M <116488361+Enma48@users.noreply.github.com>
2025-08-09 09:10:52 +0700

    fix db for localhost

--- src/db.php
+++ src/db.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', getenv('DB_HOST'));
-define('DB_USERNAME', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_NAME', getenv('DB_NAME'));
+define('DB_SERVER', 'localhost');  
+define('DB_USERNAME', 'root');     
+define('DB_PASSWORD', '');         
+define('DB_NAME', 'vul_db'); 
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 
